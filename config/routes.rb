efffe555a2050db79e159abe5ea07a04e9b912c3
Jonathan Simmons <jon@jsdev.co>
2014-05-22 09:26:04 -0500

    Update login routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
 
-  devise_for :users, controllers: { sessions: "sessions"}
+  devise_for :users, :skip => [:sessions]
 
-  devise_scope :user do
+  as :user do
     get ":uid/mark-all-read" => "users#read_all", as: :read_all_activities
     get "read/:uid" => "users#read_activity", as: :read_activity
-    get "/login" => "sessions#new", as: :login
-    get "/logout" => "sessions#destroy", as: :logout
+    get "login" => "sessions#new", as: :new_user_session
+    post 'login' => 'sessions#create', as: :user_session
+    get "logout" => "sessions#destroy", as: :logout
   end
 
   root "users#dashboard"
-
 
   resources :tasks do
     member do
